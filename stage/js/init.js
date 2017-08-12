3d99475fdf49ef108e7883ec52aff9b962f46862
linorabolini <linorabolini@gmail.com>
2017-08-12 19:05:30 +0200

    menu test

--- stage/js/init.js
+++ stage/js/init.js
@@ -1,13 +1,40 @@
 function initIncludes() {
     w3.includeHTML(function(){
-      $('body').domi();
       checkLazyElements();
     });
 }
 
-$('body').on("click", "[page]", function() {
-    $('#content').attr("w3-include-html", "./page/" + this.getAttribute("page") + '.html');
+function gotoPage(pageName) {
+    window.history.replaceState({}, pageName, "?page=" + pageName);
+    $('#content').attr("w3-include-html", "./page/" + pageName + '.html');
     initIncludes();
-});
+}
 
-initIncludes();
+function navigateToPageInUrl() {
+    var re = /[?&]page=([^&?]+)/;
+    var pageQuery = re.exec(window.location.search);
+    var pageName = pageQuery ? pageQuery[1] : 'main';
+    gotoPage(pageName);
+}
+
+function init() {
+    navigateToPageInUrl();
+    window.onpopstate = navigateToPageInUrl;
+
+    $('body').on("click", "[page]", function() {
+        var pageName = this.getAttribute("page");
+        if(window.location.search.indexOf('page=' + pageName) < 0){
+            gotoPage(pageName);
+        }
+    });
+
+    $('body').on("click", "[toggle]", function() {
+        var $this = $(this);
+        var klass = $this.attr('toggle');
+        var target = $this.attr('target') || 'body';
+
+        $(target).toggleClass(klass);
+    });
+}
+
+init();
