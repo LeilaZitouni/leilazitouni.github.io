5ffdb98212458f05b26e000dbfb510a5335c814e
linorabolini <linorabolini@gmail.com>
2017-08-14 12:53:31 +0200

    added swipe right and left

--- stage/js/init.js
+++ stage/js/init.js
@@ -1,36 +1,73 @@
 var pageCache = {};
 
+var currentPage = null;
+var pages = [
+    'main',
+    'structure',
+    'montage',
+    'communication',
+    'tournage'
+]
+
 function initIncludes(pageName) {
     w3.includeHTML(function(){
-      checkLazyElements();
-      if(window.innerWidth > 600){
-        $('.parallax').parallax();
-      }
-      $('input[page="' + pageName + '"]').prop('checked', true);
-      pageCache[pageName] = $('#content').html();
+        // load lazy loaded elements
+        checkLazyElements();
+
+        // load parallax elements
+        if(screen.width < 600){
+            $('.parallax').parallax();
+        }
+
+        // update the headline element
+        $('input[page="' + pageName + '"]').prop('checked', true);
+
+        // save the loaded page to cache
+        pageCache[pageName] = $('#content').html();
     });
 }
 
-function gotoPage(pageName) {
-    window.history.replaceState({}, pageName, "?page=" + pageName);
+function loadPage(pageName) {
     if(pageCache[pageName]) {
         $('#content').html(pageCache[pageName]);
     } else {
         $('#content').attr("w3-include-html", "./page/" + pageName + '.html');
     }
     initIncludes(pageName);
+    currentPage = pages.indexOf(pageName);
 }
 
-function navigateToPageInUrl() {
+function navigateToPageInUrl(e) {
     var re = /[?&]page=([^&?]+)/;
     var pageQuery = re.exec(window.location.search);
     var pageName = pageQuery ? pageQuery[1] : 'main';
-    gotoPage(pageName);
+
+    window.history.replaceState({}, pageName, "?page=" + pageName);
+    loadPage(pageName);
+}
+
+function gotoPrevPage() {
+    gotoPage(currentPage - 1);
+}
+
+function gotoNextPage() {
+    gotoPage(currentPage + 1);
+}
+
+function gotoPage(futurePage) {
+    if(futurePage == pages.length) { futurePage = 0; }
+    if(futurePage < 0) { futurePage = pages.length -1; }
+
+    var pageName = pages[futurePage];
+
+    window.history.pushState({}, pageName, "?page=" + pageName);
+    window.scrollTo(0,0);
+    loadPage(pageName);
 }
 
 function init() {
-    navigateToPageInUrl();
     window.onpopstate = navigateToPageInUrl;
+    navigateToPageInUrl();
 
     $('body').on("click", "[page]", function() {
         var pageName = this.getAttribute("page");
@@ -40,20 +77,66 @@ function init() {
     });
 
     $('body').on("click", "[toggle]", function() {
-        var $this = $(this);
-        var klass = $this.attr('toggle');
-        var target = $this.attr('target') || 'body';
+        var klass = this.getAttribute('toggle');
+        var target = this.getAttribute('target') || 'body';
 
         $(target).toggleClass(klass);
     });
 
     $('body').on("click", "[lazy-iframe]", function() {
-        var $this = $(this);
         var iframe = $('<iframe  width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
         iframe.attr('src', this.getAttribute('lazy-iframe'));
         this.removeAttribute('lazy-iframe');
-        $this.html(iframe);
+        $(this).html(iframe);
     });
+
+
+    // handle swipe in mobile
+    if(screen.width < 600){
+        document.addEventListener('touchstart', handleTouchStart, false);        
+        document.addEventListener('touchmove', handleTouchMove, false);
+
+        var xDown = null;                                                        
+        var yDown = null;                                                        
+
+        function handleTouchStart(evt) {          
+            xDown = evt.touches[0].clientX;                                      
+            yDown = evt.touches[0].clientY;                                      
+        };                                                
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            var xUp = evt.touches[0].clientX;                                    
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    /* left swipe */ 
+                    gotoNextPage();
+                } else {
+                    /* right swipe */
+                    gotoPrevPage();
+                }                       
+            } else {
+                if ( yDiff > 0 ) {
+                    /* up swipe */ 
+                    console.log('up swipe');
+                } else { 
+                    /* down swipe */
+                    console.log('down swipe');
+                }                                                                 
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;                                             
+        };
+    }
 }
 
 init();
